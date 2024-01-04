843e52f6d74ff1885ffc6373b84e7b9fe3e46860
Hannele Ruiz <justlemoncl@gmail.com>
2024-01-04 09:03:08 -0300

    Added basic script

---
+++ ViRe/Script.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GTA;
+using LemonUI;
+
+namespace ViRe;
+
+/// <summary>
+/// The main class of the ViRe script.
+/// </summary>
+public class ViRe : Script
+{
+    #region Fields
+
+    private ObjectPool pool = new ObjectPool();
+    private Configuration config = Configuration.Load();
+    
+    #endregion
+    
+    #region Constructors
+
+    public ViRe()
+    {
+        Tick += OnTick;
+    }
+    
+    #endregion
+    
+    #region Event Functions
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        pool.Process();
+    }
+
+    #endregion
+}
